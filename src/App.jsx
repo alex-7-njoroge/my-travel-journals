a6ec5edc0d0dcca2b781bc8518b75782852a136b
alex-7-njoroge <njorogekinyanjui02@gmail.com>
2024-10-27 01:53:41 +0300

    registration func

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import LoginForm from './components/login'
+import RegistrationForm from './components/RegistrationForm'
+import { Link ,Route,Routes} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user,setUser]=useState({})
+  function assignUser(newUser){
+    setUser(newUser)
+    console.log(user)
+  }
+  
 
   return (
 <div className='app'>
 <Routes>
+<Route path="/register" element={<RegistrationForm />} />
 <Route path="/login" element={<LoginForm
         user={user} assignUser = {assignUser} />} />
         
 </Routes>
+
 </div>
   )
 }
